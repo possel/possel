2f29d3cb7bf9e5d032151784bb664cfca7330950
Kit Barnes <kit@ninjalith.com>
2014-12-14 16:14:30 +0000

    Add setup.py and license
    And make appropriate changes to requirements.txt

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+import os.path
+
+from setuptools import setup
+
+install_requires = ['tornado',
+                    'pyzmq']
+
+classifiers = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Topic :: Communications :: Chat :: Internet Relay Chat',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3 :: Only',
+]
+
+with open(os.path.join(__file__, 'README.md')) as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    # Metadata
+    name='possel-server',
+    version='0.0.1',
+    packages=['possel'],
+    author='Kit Barnes',
+    author_email='kit@ninjalith.com',
+    description='Python-based IRC "bouncer", requires custom clients for scrollback.',
+    long_description=long_description,
+    url='https://bitbucket.org/KitB/possel/',
+    license='BSD',
+    keywords='irc quassel',
+    classifiers=classifiers,
+
+    # Non-metadata (mostly)
+    py_modules=[],
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={},
+    scripts=[],
+    package_data={},
+)
