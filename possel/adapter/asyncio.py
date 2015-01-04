484aa8aeb15d5068191d4f2d7ca6191dc93a6983
Kit Barnes <kit@ninjalith.com>
2015-01-04 15:49:56 +0000

    Move main function and related bits into possel/__init__.py

--- possel/adapter/asyncio.py
+++ possel/adapter/asyncio.py
@@ -11,34 +11,39 @@ logger = logbook.Logger(__name__)
 loop = asyncio.get_event_loop()
 
 
-def _connect_done(args, server_handler, log_handler, t):
-    reader, writer = t.result()
+def connect_done(args, server_handler, tcp_open_future):
+    reader, writer = tcp_open_future.result()
     logger.debug('Connected.')
 
-    with log_handler.applicationbound():
-        server_handler.write_function = functools.partial(_write, writer)
-        loop.call_soon(server_handler.pre_line())
-        for channel in args.channel:
-            loop.call_later(CHANNEL_JOIN_DELAY, server_handler.channels[channel].join)
+    server_handler.write_function = functools.partial(write, writer)
+    loop.call_soon(server_handler.pre_line())
+    for channel in args.channel:
+        loop.call_later(CHANNEL_JOIN_DELAY, server_handler.channels[channel].join)
 
-        asyncio.async(_read(reader, server_handler), loop=loop)
+    asyncio.async(read(reader, server_handler), loop=loop)
 
 
-def _write(writer, line):
+def write(writer, line):
     if line[-1] != '\n':
         line += '\n'
     writer.write(line.encode('utf8'))
 
 
 @asyncio.coroutine
-def _read(reader, server_handler):
+def read(reader, server_handler):
     line = yield from reader.readline()
     loop.call_soon(server_handler.handle_line, line)
-    asyncio.async(_read(reader, server_handler), loop=loop)
+    asyncio.async(read(reader, server_handler), loop=loop)
 
 
-def connect(args, server_handler, log_handler):
+def connect(args, server_handler):
     logger.debug('Connecting to server {}:{}', args.server, 6667)
-    t = asyncio.async(asyncio.open_connection(args.server, 6667), loop=loop)
-    t.add_done_callback(functools.partial(_connect_done, args, server_handler, log_handler))
+    tcp_open_future = asyncio.async(asyncio.open_connection(args.server, 6667), loop=loop)
+    tcp_open_future.add_done_callback(functools.partial(connect_done, args, server_handler))
+
+
+def main_loop():
     loop.run_forever()
+
+
+__all__ = ['connect', 'main_loop']
