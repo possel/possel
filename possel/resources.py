531ce8c0e888583359ad8bdbb6167921e0cdcc90
Kit Barnes <k.barnes@mhnltd.co.uk>
2015-09-28 17:03:52 +0100

    Add buffer and server handlers
    Now we can connect to servers, join channels, and send messages.

--- possel/resources.py
+++ possel/resources.py
@@ -23,6 +23,13 @@ class BaseAPIHandler(tornado.web.RequestHandler):
         self.set_header('Content-Type', 'application/json')
         self.controllers = controllers
 
+    def prepare(self):
+        if self.request.headers.get('Content-Type', '').startswith('application/json'):
+            self.json = json.loads(self.request.body.decode())
+
+    def get_body_argument_tuple(self, names):
+        return [self.get_body_argument(name) for name in names]
+
 
 class LinesHandler(BaseAPIHandler):
     def get(self):
@@ -47,13 +54,65 @@ class LinesHandler(BaseAPIHandler):
         self.write(json.dumps([line.to_dict() for line in lines]))
 
     def post(self):
-        buffer_id = self.get_body_argument('buffer')
-        content = self.get_body_argument('content')
+        buffer_id = self.json['buffer']
+        content = self.json['content']
 
         buffer = model.IRCBufferModel.get(id=buffer_id)
         controller = self.controllers[buffer.server_id]
 
         controller.server_handler.send_message(buffer.name, content)
+
+
+class BufferGetHandler(BaseAPIHandler):
+    def get(self, buffer_id):
+        buffers = model.IRCBufferModel.select()
+        if buffer_id != 'all':
+            buffers = buffers.where(model.IRCBufferModel.id == buffer_id)
+
+        self.write(json.dumps([buffer.to_dict() for buffer in buffers]))
+
+
+class BufferPostHandler(BaseAPIHandler):
+    def post(self):
+        server_id = self.json['server']
+        name = self.json['name']
+
+        assert name[0] in '#&+!', 'Not given a channel as buffer'
+
+        controller = self.controllers[server_id]
+        controller.server_handler.join(name)
+
+
+class ServerGetHandler(BaseAPIHandler):
+    def get(self, server_id):
+        servers = model.IRCServerModel.select()
+        if server_id != 'all':
+            servers = servers.where(model.IRCServerModel.id == server_id)
+
+        self.write(json.dumps([server.to_dict() for server in servers]))
+
+
+class ServerPostHandler(BaseAPIHandler):
+    def post(self):
+        j = self.json
+        user = model.UserDetails.create(nick=j['nick'], realname=j['realname'], username=j['username'])
+
+        server = model.IRCServerModel.create(host=j['host'], port=j['port'], secure=j['secure'], user=user)
+
+        controller = model.IRCServerController(server)
+        tornado_adapter.IRCClient.from_controller(controller).connect()
+        self.controllers[server.id] = controller
+
+
+def get_routes(controllers):
+    routes = [(r'/line', LinesHandler),
+              (r'/buffer/([0-9+]|all)', BufferGetHandler),
+              (r'/buffer', BufferPostHandler),
+              (r'/server/([0-9+]|all)', ServerGetHandler),
+              (r'/server', ServerPostHandler),
+              ]
+    routes = [route + ({'controllers': controllers},) for route in routes]
+    return [url(*route) for route in routes]
 
 
 def main():
@@ -68,8 +127,7 @@ def main():
     for client in clients.values():
         client.connect()
 
-    application = tornado.web.Application([url(r'/lines', LinesHandler, dict(controllers=controllers)),
-                                           ])
+    application = tornado.web.Application(get_routes(controllers))
     application.listen(8080)
     tornado.ioloop.IOLoop.current().start()
 
