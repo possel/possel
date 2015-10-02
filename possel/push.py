fa6f7b2971016e7f3157c41b08928fcc68bfd8fd
Kit Barnes <k.barnes@mhnltd.co.uk>
2015-10-02 19:15:07 +0100

    Add all the signals to push

--- possel/push.py
+++ possel/push.py
@@ -14,16 +14,31 @@ class ResourcePusher(tornado.websocket.WebSocketHandler):
     def send_buffer_id(self, _, buffer, server):
         self.write_message({'type': 'buffer', 'buffer': buffer.id, 'server': server.id})
 
+    def send_user_id(self, _, user, server):
+        self.write_message({'type': 'user', 'user': user.id, 'server': server.id})
+
+    def send_server_id(self, _, server):
+        self.write_message({'type': 'server', 'server': server.id})
+
+    def send_membership(self, _, membership, user, buffer):
+        self.write_message({'type': 'membership', 'membership': membership.id, 'user': user.id, 'buffer': buffer.id})
+
     def initialize(self, interfaces):
         self.interfaces = interfaces
+        self.signals = {model.new_line: self.send_line_id,
+                        model.new_buffer: self.send_buffer_id,
+                        model.new_user: self.send_user_id,
+                        model.new_server: self.send_server_id,
+                        model.new_membership: self.send_membership,
+                        }
 
     def open(self):
-        model.signal_factory(model.new_line).connect(self.send_line_id)
-        model.signal_factory(model.new_buffer).connect(self.send_buffer_id)
+        for signal, handler in self.signals.items():
+            model.signal_factory(signal).connect(handler)
 
     def on_close(self):
-        model.signal_factory(model.new_line).disconnect(self.send_line_id)
-        model.signal_factory(model.new_buffer).disconnect(self.send_buffer_id)
+        for signal, handler in self.signals.items():
+            model.signal_factory(signal).disconnect(handler)
 
 
 def main():
