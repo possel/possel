bcc9a78eac4f578443a5b87c66d8540d5d49b5ed
Kit Barnes <kit@ninjalith.com>
2015-10-03 03:38:31 +0100

    Add /connect command
    Uses argparse! How cute.

--- possel/commands.py
+++ possel/commands.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import argparse
 import logging
 
-from pircel import model
+from pircel import model, tornado_adapter
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +12,7 @@ commands = {'join',
             'query',
             'me',
             'nick',
+            'connect',
             }
 
 
@@ -50,6 +52,39 @@ class Dispatcher:
         new_nick = rest[0]
         interface = self.interfaces[buffer.server.id]
         interface.server_handler.change_nick(new_nick)
+
+    def connect(self, buffer, rest):
+        parser = argparse.ArgumentParser(prog='connect')
+        parser.add_argument('-s', '--secure', action='store_true',
+                            help='Enable ssl/tls for this server')
+        parser.add_argument('-p', '--port', default=6697,
+                            help='The port to connect on')
+        user = buffer.server.user
+        parser.add_argument('-n', '--nick', default=user.nick,
+                            help='The nick to use on this server')
+        parser.add_argument('-r', '--realname', default=user.realname,
+                            help='The real name to use on this server')
+        parser.add_argument('-u', '--username', default=user.username,
+                            help='The username to use on this server')
+        parser.add_argument('host', help='The server to connect to')
+
+        try:
+            args = parser.parse_args(rest[0].split())
+        except (SystemExit, IndexError):
+            for line in parser.format_help().splitlines():
+                model.create_line(buffer=buffer, content=line, kind='other', nick='-*-')
+            return
+
+        server = model.create_server(host=args.host,
+                                     port=args.port,
+                                     secure=args.secure,
+                                     nick=args.nick,
+                                     realname=args.realname,
+                                     username=args.username)
+
+        interface = model.IRCServerInterface(server)
+        tornado_adapter.IRCClient.from_interface(interface).connect()
+        self.interfaces[interface.server_model.id] = interface
 
 
 def main():
